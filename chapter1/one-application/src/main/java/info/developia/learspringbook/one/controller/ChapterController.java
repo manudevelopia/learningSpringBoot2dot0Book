713654deac8ac08cddad295111e32c0f312c0390
manudevelopia <manugarciarodriguez@gmail.com>
2018-10-26 19:33:38 +0200

    Remove unnecesary @Autowire

--- chapter1/one-application/src/main/java/info/developia/learspringbook/one/controller/ChapterController.java
+++ chapter1/one-application/src/main/java/info/developia/learspringbook/one/controller/ChapterController.java
@@ -2,7 +2,6 @@ package info.developia.learspringbook.one.controller;
 
 import info.developia.learspringbook.one.document.Chapter;
 import info.developia.learspringbook.one.repository.ChapterRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +13,6 @@ public class ChapterController {
 
     private final ChapterRepository chapterRepository;
 
-    @Autowired
     public ChapterController(ChapterRepository chapterRepository) {
         this.chapterRepository = chapterRepository;
     }
