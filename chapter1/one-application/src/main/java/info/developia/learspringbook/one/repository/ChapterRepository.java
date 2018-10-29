43ae83aa721a5285a41e1e4e3ce757ab9c5f85e5
manudevelopia <manugarciarodriguez@gmail.com>
2018-10-29 14:53:21 +0100

    Fix a typo, and add missing actuator configuration

--- chapter1/one-application/src/main/java/info/developia/learspringbook/one/repository/ChapterRepository.java
+++
@@ -1,8 +0,0 @@
-package info.developia.learspringbook.one.repository;
-
-import info.developia.learspringbook.one.document.Chapter;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface ChapterRepository extends ReactiveCrudRepository<Chapter, String> {
-    
-}
