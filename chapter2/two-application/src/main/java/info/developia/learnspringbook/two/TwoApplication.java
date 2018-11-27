ea9f14bf163ba902473b96c02c31039f7b7e6499
manudevelopia <manugarciarodriguez@gmail.com>
2018-11-27 07:44:29 +0100

    Add home controller

--- chapter2/two-application/src/main/java/info/developia/learnspringbook/two/TwoApplication.java
+++ chapter2/two-application/src/main/java/info/developia/learnspringbook/two/TwoApplication.java
@@ -2,11 +2,19 @@ package info.developia.learnspringbook.two;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.reactive.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 public class TwoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TwoApplication.class, args);
+    }
+
+
+    @Bean
+    HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter();
     }
 }
--- chapter2/two-application/src/main/java/info/developia/learnspringbook/two/controller/HomeController.java
+++ chapter2/two-application/src/main/java/info/developia/learnspringbook/two/controller/HomeController.java
@@ -1,29 +1,16 @@
 package info.developia.learnspringbook.two.controller;
 
 import info.developia.learnspringbook.two.service.ImageService;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.ResponseBody;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.io.IOException;
-
 @Controller
-@RequestMapping("/images")
+@RequestMapping("/")
 public class HomeController {
 
-    private static final String FILENAME = "{filename:.+}";
 
     private ImageService imageService;
 
@@ -32,31 +19,6 @@ public class HomeController {
         model.addAttribute("images", imageService.getAllImages());
 
         return Mono.just("index");
-    }
-
-    @GetMapping(value = "/" + FILENAME + "/raw", produces = MediaType.IMAGE_JPEG_VALUE)
-    @ResponseBody
-    public Mono<ResponseEntity<?>> oneRawImage(@PathVariable String filename) {
-        return imageService.findOneImage(filename)
-                .map(resource -> {
-                    try {
-                        return ResponseEntity.ok()
-                                .contentLength(resource.contentLength())
-                                .body(new InputStreamResource(resource.getInputStream()));
-                    } catch (IOException e) {
-                        return ResponseEntity.badRequest().body("Could not found: " + filename + " => " + e.getMessage());
-                    }
-                });
-    }
-
-    @PostMapping
-    public Mono<String> createFile(@RequestPart(name = "file") Flux<FilePart> files) {
-        return imageService.createNew(files).then(Mono.just("redirect:/"));
-    }
-
-    @DeleteMapping
-    public Mono<String> deleteFile(@PathVariable String filename){
-        return imageService.delete(filename).then(Mono.just("redirect:/"));
     }
 
     public HomeController(ImageService imageService) {
